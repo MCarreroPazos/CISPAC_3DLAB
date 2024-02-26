79136924494da17e92b8f27478065acd20aef55d
Miguel Carrero-Pazos <miguel.carrero.pazos@gmail.com>
2024-02-26 22:24:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-console.log('Bienvenido a la página web del LARC!');
-
 function toggleMenu() {
-    var menu = document.querySelector('nav ul');
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
     } else {
-        menu.style.display = "block";
+        menu.style.display = 'block';
     }
 }
